c15cf931df373eeb7a98ee7d069115abad113fce
raccoon91 <qsx314@gmail.com>
2023-08-20 03:40:53 +0900

    Add project setup

---
+++ .prettierrc.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  arrowParens: "avoid",
+  bracketSameLine: false,
+  bracketSpacing: true,
+  embeddedLanguageFormatting: "off",
+  htmlWhitespaceSensitivity: "css",
+  insertPragma: false,
+  jsxSingleQuote: false,
+  printWidth: 120,
+  proseWrap: "preserve",
+  quoteProps: "as-needed",
+  requirePragma: false,
+  tabWidth: 2,
+  trailingComma: "es5",
+  useTabs: false,
+};
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,5 @@
+import { Typography } from "@mui/material";
+
+export const HomePage = () => {
+  return <Typography>Home Page</Typography>;
+};
