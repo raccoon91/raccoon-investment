cf053708fd5b73431aa9c2872571da855efa5450
raccoon91 <qsx314@gmail.com>
2023-08-27 21:19:58 +0900

    Add page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { ChartPage, HomePage, SymbolPage } from "./pages";
+import { ChartPage, FavoritePage, HomePage, SymbolPage } from "./pages";
 import { Layout } from "./layouts";
 import { useGlobalStore } from "./stores";
 
@@ -16,6 +16,7 @@ export const App = () => {
           <Route element={<Layout />}>
             <Route index element={<HomePage />} />
             <Route path="symbols" element={<SymbolPage />} />
+            <Route path="favorites" element={<FavoritePage />} />
             <Route path="charts/:symbolId" element={<ChartPage />} />
           </Route>
         </Routes>
